90b96027fffa2a1fd1f9ab6a55254f3e46d3e645
fischettijw <fischettijw@gmail.com>
2024-09-16 08:21:28 -0400

    09/16/2024   08:21 AM

--- sandbox/kde_test.py
+++ sandbox/kde_test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 heights = [150, 155, 160, 162, 165, 167, 170, 172, 175, 178, 180, 182, 185, 190]
 
 # Create the KDE plot
-sns.kdeplot(heights, fill=True)
+sns.kdeplot(heights, fill=True, gridsize = 800)
 
 # Label the axes
 plt.xlabel('Height (cm)')
